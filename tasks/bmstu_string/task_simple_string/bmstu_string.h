5f1fb8760289b4515a00b9fef32c4592b79c4dce
diddy <random@bk.ru>
2026-02-15 19:57:17 +0300

    simple string + sso

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstring>	// std::memcpy
 #include <exception>
 #include <iostream>
 
@@ -18,82 +19,197 @@ class simple_basic_string
 {
    public:
 	/// Конструктор по умолчанию
-	simple_basic_string() : ptr_(new T[1]{0}), size_(0) {}
+	simple_basic_string()
+	{
+		ptr_ = new T[1];
+		ptr_[0] = '\0';
+		size_ = 0;
+	}
 
-	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size) {}
+	simple_basic_string(size_t size)
+	{
+		T filler = static_cast<T>(' ');
+		ptr_ = new T[size + 1];
+		size_ = size;
+		for (size_t i = 0; i < size_; ++i)
+		{
+			ptr_[i] = filler;
+		}
+		ptr_[size_] = '\0';
+	}
 
 	simple_basic_string(std::initializer_list<T> il)
-		: ptr_(new T[il.size() + 1]), size_(il.size())
 	{
+		ptr_ = new T[il.size() + 1];
+		size_ = il.size();
+		size_t i = 0;
+		for (T c : il)
+		{
+			ptr_[i] = c;
+			i++;
+		}
+		ptr_[size_] = '\0';
 	}
 
 	/// Конструктор с параметром си-с
-	simple_basic_string(const T* c_str) {}
+	simple_basic_string(const T* c_str)
+	{
+		size_ = strlen_(c_str);
+		ptr_ = new T[size_ + 1];
+		std::memcpy(ptr_, c_str, (size_ + 1) * sizeof(T));
+	}
 
 	/// Конструктор копирования
-	simple_basic_string(const simple_basic_string& other) {}
+	simple_basic_string(const simple_basic_string& other)
+	{
+		size_ = other.size();
+		ptr_ = new T[size_ + 1];
+		std::memcpy(ptr_, other.c_str(), (size_ + 1) * sizeof(T));
+	}
 
 	/// Перемещающий конструктор
-	simple_basic_string(simple_basic_string&& dying) {}
+	simple_basic_string(simple_basic_string&& dying)
+	{
+		ptr_ = dying.ptr_;
+		size_ = dying.size_;
+		dying.ptr_ = new T[1];
+		dying.ptr_[0] = '\0';
+		dying.size_ = 0;
+	}
 
 	/// Деструктор
-	~simple_basic_string() {}
+	~simple_basic_string() { clean_(); }
 
 	/// Геттер на си-строку
 	const T* c_str() const { return ptr_; }
 
-	size_t size() const { return 0; }
+	size_t size() const { return size_; }
 
 	/// Оператор копирующего присваивания
 	simple_basic_string& operator=(simple_basic_string&& other)
 	{
+		if (this == &other)
+			return *this;
+		clean_();
+		ptr_ = other.ptr_;
+		size_ = other.size_;
+		other.ptr_ = new T[1];
+		other.ptr_[0] = '\0';
+		other.size_ = 0;
+
 		return *this;
 	}
 
 	/// Оператор копирующего присваивания си строки
-	simple_basic_string& operator=(const T* c_str) { return *this; }
+	simple_basic_string& operator=(const T* c_str)
+	{
+		size_t new_size = strlen_(c_str);
+		T* new_ptr = new T[new_size + 1];
+		std::memcpy(new_ptr, c_str, (new_size + 1) * sizeof(T));
+
+		clean_();
+		ptr_ = new_ptr;
+		size_ = new_size;
+		return *this;
+	}
 
 	/// Оператор копирующего присваивания
 	simple_basic_string& operator=(const simple_basic_string& other)
 	{
+		size_t new_size = other.size_;
+		T* new_ptr = new T[new_size + 1];
+		std::memcpy(new_ptr, other.ptr_, (new_size + 1) * sizeof(T));
+		clean_();
+		ptr_ = new_ptr;
+		size_ = new_size;
 		return *this;
 	}
 
 	friend simple_basic_string<T> operator+(const simple_basic_string<T>& left,
 											const simple_basic_string<T>& right)
 	{
-		return {};
+		simple_basic_string<T> result(left);
+		result += right;
+		return result;
 	}
 
 	template <typename S>
 	friend S& operator<<(S& os, const simple_basic_string& obj)
 	{
+		if (obj.size_ > 0)
+		{
+			os.write(obj.ptr_, obj.size_);
+		}
 		return os;
 	}
 
 	template <typename S>
 	friend S& operator>>(S& is, simple_basic_string& obj)
 	{
+		obj.clean_();
+		T c;
+		while (is.get(c))
+		{
+			obj += c;
+		}
 		return is;
 	}
 
 	simple_basic_string& operator+=(const simple_basic_string& other)
 	{
+		size_t new_size = size_ + other.size_;
+		T* new_ptr = new T[new_size + 1];
+		std::memcpy(new_ptr, ptr_, size_ * sizeof(T));
+		std::memcpy(new_ptr + size_, other.ptr_, (other.size_ + 1) * sizeof(T));
+		clean_();
+		ptr_ = new_ptr;
+		size_ = new_size;
 		return *this;
 	}
 
-	simple_basic_string& operator+=(T symbol) { return *this; }
+	simple_basic_string& operator+=(T symbol)
+	{
+		size_t new_size = size_ + 1;
+		T* new_ptr = new T[new_size + 1];
+		std::memcpy(new_ptr, ptr_, size_ * sizeof(T));
+		new_ptr[size_] = symbol;
+		new_ptr[size_ + 1] = '\0';
+		clean_();
+		ptr_ = new_ptr;
+		size_ = new_size;
+		return *this;
+	}
 
 	T& operator[](size_t index) noexcept { return *(ptr_ + index); }
 
-	T& at(size_t index) { throw std::out_of_range("Wrong index"); }
-
+	T& at(size_t index)
+	{
+		if (index >= size_)
+		{
+			throw std::out_of_range("Wrong index");
+		}
+		return *(ptr_ + index);
+	}
 	T* data() { return ptr_; }
 
    private:
-	static size_t strlen_(const T* str) { return 0; }
+	static size_t strlen_(const T* str)
+	{
+		size_t ans = 0;
+		while (*str != '\0')
+		{
+			ans++;
+			str++;
+		}
+		return ans;
+	}
 
-	void clean_() {}
+	void clean_()
+	{
+		delete[] ptr_;
+		ptr_ = nullptr;
+		size_ = 0;
+	}
 
 	T* ptr_ = nullptr;
 	size_t size_;
--- tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
+++ tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstring>	// для std::memcpy
 #include <exception>
 #include <iostream>
 
@@ -49,76 +50,372 @@ class basic_string
 		return is_long() ? data_.long_str.ptr : data_.short_str.buffer;
 	}
 
-	const T* get_ptr() const { return nullptr; }
+	const T* get_ptr() const
+	{
+		return is_long() ? data_.long_str.ptr : data_.short_str.buffer;
+	}
 
-	size_t get_size() const { return 0; }
+	size_t get_size() const
+	{
+		return is_long() ? data_.long_str.size : data_.short_str.size;
+	}
 
-	size_t get_capacity() const { return 0; }
+	size_t get_capacity() const
+	{
+		return is_long() ? data_.long_str.capacity : SSO_CAPACITY;
+	}
 
    public:
-	basic_string() {}
+	basic_string()
+	{
+		is_long_ = false;
+		data_.short_str.buffer[0] = '\0';
+		data_.short_str.size = 0;
+	}
 
-	basic_string(size_t size) {}
+	basic_string(size_t size)
+	{
+		T filler = static_cast<T>(' ');
 
-	basic_string(std::initializer_list<T> il) {}
+		if (size <= SSO_CAPACITY)
+		{
+			is_long_ = false;
+			data_.short_str.size = static_cast<unsigned char>(size);
+			for (size_t i = 0; i < size; ++i)
+			{
+				data_.short_str.buffer[i] = filler;
+			}
+			data_.short_str.buffer[size] = '\0';
+		}
+		else
+		{
+			is_long_ = true;
+			data_.long_str.size = size;
+			data_.long_str.capacity = size + 1;
+			data_.long_str.ptr = new T[get_capacity()];
+			for (size_t i = 0; i < size; ++i)
+			{
+				data_.long_str.ptr[i] = filler;
+			}
+			data_.long_str.ptr[size] = '\0';
+		}
+	}
 
-	basic_string(const T* c_str) {}
+	basic_string(std::initializer_list<T> il)
+	{
+		size_t size = il.size();
+		if (size <= SSO_CAPACITY)
+		{
+			is_long_ = false;
+			data_.short_str.size = static_cast<unsigned char>(size);
+			size_t i = 0;
+			for (T c : il)
+			{
+				data_.short_str.buffer[i++] = c;
+			}
+			data_.short_str.buffer[size] = '\0';
+		}
+		else
+		{
+			is_long_ = true;
+			data_.long_str.size = size;
+			data_.long_str.capacity = size + 1;
+			data_.long_str.ptr = new T[get_capacity()];
 
-	basic_string(const basic_string& other) {}
+			size_t i = 0;
+			for (T c : il)
+			{
+				data_.long_str.ptr[i++] = c;
+			}
+			data_.long_str.ptr[size] = '\0';
+		}
+	}
 
-	basic_string(basic_string&& dying) noexcept {}
+	basic_string(const T* c_str)
+	{
+		size_t len = strlen_(c_str);
 
-	~basic_string() {}
+		if (len <= SSO_CAPACITY)
+		{
+			is_long_ = false;
+			std::memcpy(data_.short_str.buffer, c_str, len * sizeof(T));
+			data_.short_str.buffer[len] = '\0';
+			data_.short_str.size = static_cast<unsigned char>(len);
+		}
+		else
+		{
+			is_long_ = true;
+			data_.long_str.capacity = len + 1;
+			data_.long_str.size = len;
+			data_.long_str.ptr = new T[get_capacity()];
+			std::memcpy(data_.long_str.ptr, c_str, (len + 1) * sizeof(T));
+		}
+	}
 
-	const T* c_str() const { return nullptr; }
+	basic_string(const basic_string& other)
+	{
+		is_long_ = other.is_long_;
+		if (is_long())
+		{
+			size_t cap = other.data_.long_str.capacity;
+			data_.long_str.ptr = new T[cap];
+			data_.long_str.size = other.data_.long_str.size;
+			data_.long_str.capacity = cap;
+			std::memcpy(data_.long_str.ptr, other.data_.long_str.ptr,
+						(data_.long_str.size + 1) * sizeof(T));
+		}
+		else
+		{
+			data_.short_str = other.data_.short_str;
+		}
+	}
 
-	size_t size() const { return 0; }
+	basic_string(basic_string&& dying) noexcept
+	{
+		is_long_ = dying.is_long_;
+		if (is_long())
+		{
+			data_.long_str = dying.data_.long_str;
+			dying.data_.long_str.ptr = nullptr;
+			dying.data_.long_str.size = 0;
+		}
+		else
+		{
+			data_.short_str = dying.data_.short_str;
+		}
+		dying.is_long_ = false;
+		dying.data_.short_str.size = 0;
+	}
 
-	bool is_using_sso() const { return false; }
+	~basic_string() { clean_(); }
 
-	size_t capacity() const { return 0; }
+	const T* c_str() const { return get_ptr(); }
 
-	basic_string& operator=(basic_string&& other) noexcept { return *this; }
+	size_t size() const
+	{
+		return is_long() ? data_.long_str.size
+						 : static_cast<size_t>(data_.short_str.size);
+	}
 
-	basic_string& operator=(const T* c_str) { return *this; }
+	bool is_using_sso() const { return !is_long(); }
 
-	basic_string& operator=(const basic_string& other) { return *this; }
+	size_t capacity() const
+	{
+		return is_long() ? data_.long_str.capacity : SSO_CAPACITY;
+	}
+
+	basic_string& operator=(const basic_string& other)
+	{
+		if (this == &other)
+			return *this;
+
+		if (other.is_long())
+		{
+			size_t cap = other.get_capacity();
+			size_t size = other.get_size();
+			T* new_ptr = new T[cap];
+			std::memcpy(new_ptr, other.get_ptr(), (size + 1) * sizeof(T));
+			clean_();
+			data_.long_str.size = size;
+			data_.long_str.capacity = cap;
+			data_.long_str.ptr = new_ptr;
+			is_long_ = true;
+		}
+		else
+		{
+			clean_();
+			data_.short_str = other.data_.short_str;
+			is_long_ = false;
+		}
+		return *this;
+	}
+
+	basic_string& operator=(const T* c_str)
+	{
+		size_t len = strlen_(c_str);
+
+		if (len <= SSO_CAPACITY)
+		{
+			clean_();
+			is_long_ = false;
+			std::memcpy(data_.short_str.buffer, c_str, len * sizeof(T));
+			data_.short_str.buffer[len] = '\0';
+			data_.short_str.size = static_cast<unsigned char>(len);
+		}
+		else
+		{
+			size_t new_cap = len + 1;
+			T* new_ptr = new T[new_cap];
+			std::memcpy(new_ptr, c_str, (len + 1) * sizeof(T));
+			clean_();
+			data_.long_str.capacity = new_cap;
+			data_.long_str.ptr = new_ptr;
+			data_.long_str.size = len;
+			is_long_ = true;
+		}
+		return *this;
+	}
+
+	basic_string& operator=(basic_string&& other)
+	{
+		if (this == &other)
+			return *this;
+		clean_();
+		is_long_ = other.is_long_;
+		data_ = other.data_;
+
+		if (other.is_long_)
+		{
+			other.data_.long_str.ptr = nullptr;
+			other.data_.long_str.size = 0;
+		}
+		else
+		{
+			other.data_.short_str.size = 0;
+			other.data_.short_str.buffer[0] = '\0';
+		}
+		other.is_long_ = false;
+
+		return *this;
+	}
 
 	friend basic_string<T> operator+(const basic_string<T>& left,
 									 const basic_string<T>& right)
 	{
-		return {};
+		basic_string<T> result(left);
+		result += right;
+		return result;
 	}
 
 	template <typename S>
 	friend S& operator<<(S& os, const basic_string& obj)
 	{
+		if (obj.size())
+			os.write(obj.data(), obj.get_size());
 		return os;
 	}
 
 	template <typename S>
 	friend S& operator>>(S& is, basic_string& obj)
 	{
+		obj.clean_();
+		T c;
+		while (is.get(c))
+		{
+			obj += c;
+		}
 		return is;
 	}
 
-	basic_string& operator+=(const basic_string& other) { return *this; }
-
-	basic_string& operator+=(T symbol) { return *this; }
-
-	T& operator[](size_t index) noexcept
+	basic_string& operator+=(const basic_string& other)
 	{
-		static T dummy;
-		return dummy;
+		size_t current_size = get_size();
+		size_t other_size = other.get_size();
+		size_t new_size = current_size + other_size;
+		if (new_size > get_capacity())
+		{
+			size_t new_cap = new_size + 1;
+			T* new_ptr = new T[new_cap];
+
+			std::memcpy(new_ptr, get_ptr(), current_size * sizeof(T));
+			std::memcpy(new_ptr + current_size, other.get_ptr(),
+						(other_size + 1) * sizeof(T));
+
+			clean_();
+
+			is_long_ = true;
+			data_.long_str.ptr = new_ptr;
+			data_.long_str.capacity = new_cap;
+		}
+		else
+		{
+			// копируем данные из other
+			std::memcpy(get_ptr() + current_size, other.get_ptr(),
+						(other_size + 1) * sizeof(T));
+		}
+		if (is_long())
+		{
+			data_.long_str.size = new_size;
+		}
+		else
+		{
+			data_.short_str.size = static_cast<unsigned char>(new_size);
+		}
+		return *this;
 	}
 
-	T& at(size_t index) { throw std::out_of_range("Wrong index"); }
+	basic_string& operator+=(T symbol)
+	{
+		size_t current_size = get_size();
+		size_t new_size = current_size + 1;
+		if (new_size > get_capacity())
+		{
+			size_t new_cap = new_size + 1;
+			T* new_ptr = new T[new_cap];
+			std::memcpy(new_ptr, get_ptr(), current_size * sizeof(T));
+			std::memcpy(new_ptr + current_size, &symbol, sizeof(T));
+			new_ptr[new_size] = '\0';
 
-	T* data() { return nullptr; }
+			clean_();
+
+			is_long_ = true;
+			data_.long_str.ptr = new_ptr;
+			data_.long_str.size = new_size;
+			data_.long_str.capacity = new_cap;
+		}
+		else
+		{
+			get_ptr()[current_size] = symbol;
+			get_ptr()[current_size + 1] = '\0';
+			if (is_long())
+			{
+				data_.long_str.size = new_size;
+			}
+			else
+			{
+				data_.short_str.size = static_cast<unsigned char>(new_size);
+			}
+		}
+		return *this;
+	}
+
+	T& operator[](size_t index) noexcept { return get_ptr()[index]; }
+
+	T& at(size_t index)
+	{
+		if (index >= get_size())
+		{
+			throw std::out_of_range("Wrong index");
+		}
+		return get_ptr()[index];
+	}
+
+	T* data() { return get_ptr(); }
+
+	const T* data() const { return get_ptr(); }
 
    private:
-	static size_t strlen_(const T* str) { return 0; }
+	static size_t strlen_(const T* str)
+	{
+		size_t len = 0;
+		while (*str != '\0')
+		{
+			str++;
+			len++;
+		}
+		return len;
+	}
 
-	void clean_() {}
+	void clean_()
+	{
+		if (is_long())
+		{
+			delete[] data_.long_str.ptr;
+			data_.long_str.ptr = nullptr;
+		}
+		is_long_ = false;
+		data_.short_str.size = 0;
+		data_.short_str.buffer[0] = '\0';
+	}
 };
 }  // namespace bmstu
